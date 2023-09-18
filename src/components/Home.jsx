13cba39b0873e1108757be00b38104b141877159
AJ1732 <ejemeniboi@gmail.com>
2023-09-18 21:54:05 +0100

    Got Pexels API

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { signOut } from 'firebase/auth'
 import { database } from './FirebaseConfig'
 import { useNavigate } from 'react-router-dom'
 
 
 const Home = () => {
+  const [photosList, setPhotosList] = useState([])
+
+  const options = {
+    method: 'GET',
+    headers:{
+      Accept: 'application/json',
+      Authorization: ''
+    }
+  }
+  // find a way to use the env
+  async function getPhotos() {
+    const data = await fetch("https://api.pexels.com/v1/search?query=nature&per_page=1", options)
+    const dataObj = await data.json()
+    setPhotosList(dataObj);
+  }
+
+  useEffect(() => {
+    getPhotos()
+  }, []);
+
+  console.log(photosList);
+
+
+
   const history = useNavigate()
 
   const handleClick = () => {
     signOut(database).then( value => {
-      console.log(value, "value")
+      // console.log(value, "value")
       history("/")
     })
   }
 
   return (
     <div className='bg-bg-orange w-full h-calc | flex flex-col justify-center items-center gap-8'>
-      <h1>Home</h1>
       <button onClick={handleClick} className='text-orange font-semibold rounded-md py-2  | absolute top-6 right-8'>Sign Out</button>
+      <h1>Home</h1>
+      <div>
+
+      </div>
     </div>
   )
 }
