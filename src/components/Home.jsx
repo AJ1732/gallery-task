372dbbf25217d02047f53bb38a3a0309104b86df
AJ1732 <ejemeniboi@gmail.com>
2023-09-19 05:37:24 +0100

    Added Loading feature

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,16 +17,16 @@ const Home = () => {
   }
   // find a way to use the env
   async function getPhotos() {
-    const data = await fetch("https://api.pexels.com/v1/search?query=nature&per_page=1", options)
+    const data = await fetch("https://api.pexels.com/v1/curated?query=nature&per_page=12", options)
     const dataObj = await data.json()
-    setPhotosList(dataObj);
+    setPhotosList(dataObj.photos);
   }
 
   useEffect(() => {
     getPhotos()
   }, []);
 
-  console.log(photosList);
+  // console.log(photosList);
 
 
 
@@ -39,12 +39,25 @@ const Home = () => {
     })
   }
 
-  return (
-    <div className='bg-bg-orange w-full h-calc | flex flex-col justify-center items-center gap-8'>
-      <button onClick={handleClick} className='text-orange font-semibold rounded-md py-2  | absolute top-6 right-8'>Sign Out</button>
-      <h1>Home</h1>
-      <div>
+  const photosListElement = photosList.map( photo => (
+    <div key={photo.id}>
+      <img 
+        src={photo.src.tiny} 
+        className='rounded' 
+        alt={photo.alt} />
+    </div>
+  ))
 
+  return (
+    <div className='bg-bg-orange w-full h-max p-8 | flex flex-col justify-center items-center gap-8'>
+      <button onClick={handleClick} className='text-orange font-semibold rounded-md py-2  | absolute top-6 right-8'>Sign Out</button>
+      <h1 className='text-black text-2xl'>Drag & Drop the images wherever you like</h1>
+      <div className='w-full grid sm:grid-cols-4 grid-cols-2 content-center gap-4 '>
+        {
+          photosList.length > 0? 
+          photosListElement: 
+          <h2 className='w-full text-center text-3xl m-auto h-calc p-20'>Loading...</h2>
+        }
       </div>
     </div>
   )
